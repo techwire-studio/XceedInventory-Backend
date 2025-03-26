e92f7cfac49550f751ad4b71c1237c1ee46cfa54
Miit Daga <miitcodes27@gmail.com>
2025-03-26 19:37:27 +0530

    fixed merge conflicts

--- src/index.js
+++ src/index.js
@@ -2,27 +2,20 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 import orderRoutes from "./routes/orderRoutes.js";
-<<<<<<< Updated upstream
-=======
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
->>>>>>> Stashed changes
 import cors from 'cors';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-<<<<<<< Updated upstream
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
-}
-);
+});
 
-=======
-app.use(cookieParser());
->>>>>>> Stashed changes
 // Routes
 app.use('/api/products', productRoutes);
 app.use("/api/orders", orderRoutes);
